2e035034024db3cdd456ca3d4101d2495ac09095
FarhanAliShah <fsgraphicsdesigner@gmail.com>
2020-06-07 22:16:58 +0500

    JS Assignments

---
+++ app.js
@@ -0,0 +1,440 @@
+//JS Assignments by Syed Farhan Ali Shah
+
+
+//Chapter 1 Task no: 1
+// alert("Assalam o Alaikum");
+
+
+
+//Chapter 1 Task no: 2
+// alert("Error! Please enter a valid password.");
+
+
+
+//Chapter 1 Task no: 3
+// alert("\n" + "Welcome to JS Land..." + "\n" + "Happy Coding!");
+
+
+
+//Chapter 1 Task no: 4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding" + "\n" + "Prevent this page from creating additional dialogs.");
+
+
+
+//Chapter 1 Task no: 5
+// console.log("hello... I can ru JS through my web browser's console");
+
+            
+            //END CHAPTER NO: 1
+
+
+
+
+
+
+//Chapter 2 Task no: 1
+// var username;
+
+
+
+//Chapter 2 Task no: 2
+// var myName = "Syed Farhan Ali Shah";
+
+
+
+//Chapter 2 Task no: 3
+// var message = "hello world";
+// alert(message);
+
+
+
+//Chapter 2 Task no: 4
+// var stdData = "Jhone Doe";
+// alert(stdData);
+// var stdData = "15 years old";
+// alert(stdData);
+// var stdData = "Certified Mobile Application Development";
+// alert(stdData);
+
+
+
+//Chapter 2 Task no: 5
+// alert("PIZZA"+ "\n" + "PIZZ"+ "\n" + "PIZ"+ "\n" + "PI"+ "\n" + "P");
+
+
+//Chapter 2 Task no: 6
+// var email = "syedfarhanali492@gmail.com";
+// alert("My email address is " + email);
+
+
+
+//Chapter 2 Task no: 7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the book " + book);
+
+
+
+//Chapter 2 Task no: 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+
+//Chapter 2 Task no: 9
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(a);
+
+
+            //END CHAPTER NO: 2
+
+
+
+
+
+
+//Chapter 3 Task no: 1
+// var age = "19";
+// alert("I am " + age +  " years old");
+
+
+
+//Chapter 3 Task no: 2
+
+
+
+
+
+
+//Chapter 3 Task no: 3
+// var birthYear = 1998;
+// document.write("My birth year is " + birthYear); 
+// document.write(" Data type of my declared variable is number");
+
+
+
+//Chapter 3 Task no: 4
+// var visitorName = "John Doe";
+// var productTitle = " ordered";
+// var quantity = " 5 T-shirt(s)";
+// document.write(visitorName);
+// document.write(productTitle);
+// document.write(quantity + " on XYZ Clothing store");
+
+
+        //END CHAPTER NO: 3
+
+
+
+
+// Lecture no: 4
+// // //VARIABLE NAMES: LEGAL & ILLEGAL
+
+// Task no:1
+// // var one,two,three;
+// // //2
+// // // ILLEGAL
+// // var 1one;
+// // var "two";
+// // var @three;
+// // var &four;
+// // var %five;
+// // // LEGAL
+// // var one;
+// // var _two;
+// // var th_ree;
+// // var four4
+// // var five_5
+// //3
+// //A
+// document.write("<h1>“Rules for naming JS variables</h1>");
+// //B
+// document.write("<br>Variable names can only contain letters,digits,underscores and dollar signs.");
+// //C
+// document.write("<br>Variables must begin with a letter,$ or case sensitive For example <b>$name, _name</b> or <b>name</b>")
+// //D
+// document.write("<br>Variable names are case Sensitive");
+// //E
+// document.write("<br>Variable names should not be JS keyword");
+
+
+            // END CHAPTER NO: 4
+
+
+
+
+
+// Lecture no; 5
+// //Math Expressions
+
+
+// Task no:1
+
+// // Add
+// var a=1;
+// var b=2;
+// var c=a+b;
+// document.write(`Sum of ${a} and ${b} is ${c}`);
+
+
+
+// Task no:2
+//  Sub
+// var a=1;
+// var b=2;
+// var c=a-b;
+// document.write(`substract of ${a} and ${b} is ${c}`);
+//  Mul
+// var a=1;
+// var b=2;
+// var c=a*b;
+// document.write(`Multiply of ${a} and ${b} is ${c}`);
+//  Div
+// var a=1;
+// var b=2;
+// var c=a/b;
+// document.write(`division of ${a} and ${b} is ${c}`);
+
+
+
+
+// Task no:3
+// a
+// var value;
+// b
+// document.write(`<br />Value after variable declaration is ${value}`);
+// c
+// value=5;
+// d
+// document.write(`<br />Initial value ${value}`);
+// e
+// value++;
+// f
+// document.write(`<br />Value after increment is ${value}`);
+// //g
+// value = value+7;
+// //H
+// document.write(`<br />Value after addition is 7`);
+// //i
+// value--;
+// //j
+// document.write(`<br />Value after decrement is ${value}`);
+// //k
+// value=value%3;
+// //L
+// document.write(`<br />The remainder is ${value}`)
+
+
+
+// Task no:4
+// var ticket = 600;
+// ticket=ticket*5;
+// document.write(`<br/>Total cost to buy 5 tickets to a movie is ${ticket} PKR`);
+
+
+
+
+// Ts no:5
+// document.write(`<br/>table of 4`)
+// document.write(`<br/>4x1=4`);
+// document.write(`<br/>4x2=8`);
+// document.write(`<br/>4x3=12`);
+// document.write(`<br/>4x4=16`);
+// document.write(`<br/>4x5=20`);
+// document.write(`<br/>4x6=24`);
+// document.write(`<br/>4x7=28`);
+// document.write(`<br/>4x8=32`);
+// document.write(`<br/>4x9=36`);
+// document.write(`<br/>4x10=40`);
+
+
+
+// Task no:6
+// var celsiusTemp=52;
+// var fahrenheit=(celsiusTemp*9/5)+32;
+// document.write('<br/>'+fahrenheit);
+// var fahrenheitTemp = 102;
+// var celsius=(fahrenheitTemp-32)*5/9;
+// document.write('<br/>'+celsius);
+
+
+
+// Task no:7
+// var priceItem1=650;
+// var quantityaItem1=3;
+// var priceItem2=100;
+// var quantityaItem2=7;
+// var shipCharges=100;
+// document.writeln("<br/>Price of item 1 is "+priceItem1);
+// document.writeln("<br/>Quantity of the item 1 is "+quantityaItem1);
+// document.writeln("<br/>Price of item 2 is "+priceItem2);
+// document.writeln("<br/>Quantity of the item 2 is "+quantityaItem2);
+// document.writeln("<br/>Shipping Charges "+shipCharges);
+// var total = (priceItem1*quantityaItem1)+(priceItem2*quantityaItem2)+shipCharges;
+// document.writeln("<br/>Total cost of your order "+total);
+
+
+
+
+// Task no:8
+// var obtainedmark=804;
+// var totalmarks=980;
+// var percentage = (100*obtainedmark)/totalmarks;
+// document.write("<br/> Total Marks = "+totalmarks);
+// document.write("<br/> Marks Obtained = "+obtainedmark);
+// document.write("<br/> Percentage = "+percentage);
+
+
+
+
+// Task no:9
+// var exchangeCurrenncy = (10*104.80) + (28*25);
+// document.write("<br/>Total Currency"+exchangeCurrenncy);
+
+
+
+// Task no:10
+// var someNumber=10;
+// (someNumber+5)*10/2;
+// document.write("<br/>Arthmetic "+someNumber);
+
+
+
+
+// Task no:11
+// var currentYear=2020;
+// var birthDate=1998;
+// var  yourAge= currentYear-birthDate;
+// document.write("<br/>Current Year"+currentYear);
+// document.write("<br/>Birth Year"+birthYear);
+// document.write("<br/> Your Age is "+yourAge);
+
+
+
+// Task no:12 
+
+
+
+
+// Task no:13
+// var favouritesnake="Chocolate chip";
+// var currentage=15;
+// var estimeteage=65;
+// var amountofsnakeperday=3;
+// document.write("<br/> Favourite Snack :"+favouritesnake);
+// document.write("<br/> Current Age is "+currentage);
+// document.write("<br/> Estimate Maximum Age "+estimeteage);
+// document.write("<br/> Amount of Snakes Per day "+amountofsnakeperday);
+// document.write("<br/> You will need 150 "+favouritesnake+" to last you until the ripe old age of "+estimeteage);
+
+
+            // LECTURE NO:5 END
+
+
+
+
+
+            
+// Lacture no:6-9 
+// //MATH EXPRESSIONS 
+
+// Task no:1
+
+// var num=10;
+// document.write("<br/> Result:");
+// document.write("<br/> The value of a is :"+num);
+// document.write("<br/>''''''''''''''''''''''''''''''''''''");
+// ++num;
+// document.write("<br/> The value of ++a is : "+num);
+// document.write("<br/> Now the value of a is : "+num);
+// num++;
+// document.write("<br/> The value of a++ is : "+num);
+// document.write("<br/> Now the value of a is : "+num);
+// --num;
+// document.write("<br/> The value of --a is : "+num);
+// document.write("<br/> Now the value of a is : "+num);
+// num--;
+// document.write("<br/> The value of a-- is : "+num);
+// document.write("<br/> Now the value of a is : "+num);
+
+
+
+// Task no:2
+// var a=2,b=1;
+// var result = --a - --b + ++b + b--
+//     // 3       1  -  0 +   1 + 1 
+
+
+
+// Task no:3
+// document.write("<br/>a is "+a);
+// document.write("<br/>b is "+b);
+// document.write("<br/> Result is "+result);
+// function greetMessage(){
+//     var inputmessage=document.getElementById('greet').value;
+//     document.getElementById('msg').innerHTML="Greeting Sir "+inputmessage;
+// }
+
+
+
+// Task no:5
+// function generateTable(){
+//     let value = document.getElementById('multinum').value;
+//     if(value===""){
+//         value=5
+//         for (let i = 1; i <= 10; i++) {
+//             document.write("<br> "+value+" x " +i+" = "+value*i+"<br/>")
+//         }
+//     }else{
+//         for (let i = 1; i <= 10; i++) {
+//             document.write("<br> "+value+" x " +i+" = "+value*i+"<br/>")
+//         }
+//     }
+// }
+
+
+
+// Task no:6
+//a
+var english="English";
+var Isl="Islamiat";
+var physics="Physics";
+//b
+var totalmark=100;
+//c
+var eng=54;
+//d
+var isl=54;
+var phy=48;
+//e
+function showTable(){
+    //english
+    englishPer=(100*eng)/100;
+    document.getElementById('sub1').innerHTML=english;
+    document.getElementById('total1').innerHTML=totalmark;
+    document.getElementById('obt1').innerHTML=eng;
+    document.getElementById('mp1').innerHTML=englishPer+"%";
+    //islamiat
+    islPer=(100*isl)/100;
+    document.getElementById('sub2').innerHTML=Isl;
+    document.getElementById('total2').innerHTML=totalmark;
+    document.getElementById('obt2').innerHTML=isl;
+    document.getElementById('mp2').innerHTML=islPer+"%";
+    //physics
+    phyPer=(100*phy)/100;
+    document.getElementById('sub3').innerHTML=physics;
+    document.getElementById('total3').innerHTML=totalmark;
+    document.getElementById('obt3').innerHTML=phy;
+    document.getElementById('mp3').innerHTML=phyPer+"%";
+    
+    totalmark*3;
+    document.getElementById('totalmarks').innerHTML=totalmark;
+    
+    var obtTotal=eng+isl+phy;
+    document.getElementById('obttol').innerHTML=obtTotal;
+
+    var calculatePer=(100*eng+phy+isl)/100;
+    document.getElementById('per').innerHTML=calculatePer;
+}
+
+
+            // LECTURE NO: 6-9 END
